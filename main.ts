8c2da287502e0f37945f254f76eeb29cb3fda068
Massou1 <120166960+Massou1@users.noreply.github.com>
2023-01-13 09:49:54 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+def pot ():
+    val = pinl.read_digital():
+    intensite = int(9*val.1023)
+    retour intensite
+
+    def colonned(P,Q):
+        for i in range (S):
+        display.set_pixel(P,y,Q)
+
+        def aller (R,S)
+        for i in range (R)
+            colonned (i, S-R+I)
+
+            while True : 
+
+            for i in range (1,5)
+            aller (i, intensite)
+            slee(500)
+            displayclear()
